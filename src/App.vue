4169301306e6d6927a051b5c65565c054d1fb985
LAURA YAMILE ORDONEZ BARRERA <laura.yamile.barrera@ibm.com>
2019-07-26 08:24:01 -0500

    CHANGE

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar dark color="#015372" app>
+        <v-toolbar-title>
+        <v-icon>fa-film</v-icon>
+        <span>&nbsp MOVIES</span>
+        </v-toolbar-title>
+        
+    </v-toolbar>
+
+    <v-content>
+      <v-container  grid-list-md text-xs-center style="max-widht:1200;">
+      <router-view/>
+    </v-container>
+  </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+export default {
+  name: 'App',
+  data () {
+    return {
+      //
+    }
+  }
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
